b544b8b32a4db497843393e58f62f337dd814fb7
Patrick Kelso <psk@puppetlabs.com>
2015-01-20 12:55:51 +1100

    defaults

--- defaults.osx.sh
+++ defaults.osx.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 defaults write com.apple.Finder FxPreferredViewStyle clmv
+launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist
+defaults write -g NSWindowResizeTime -float 0.003
+sudo defaults write /System/Library/LaunchAgents/com.apple.notificationcenterui KeepAlive -bool false
 
 killall Finder
+killall NotificationCenter
